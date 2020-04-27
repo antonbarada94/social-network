dfffdbb6ab24cf791f90c0900bc6b43d3b02ad57
Anton Sergushkin <antonbarada94@gmail.com>
2020-04-27 18:33:25 +0300

    creating profile page

---
+++ src/components/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Background = () => {
+   return (
+      <div className = 'background'>
+         <div className = "mini-description">
+            <p className = 'username'>Firstname + Lastname</p>
+            <p className = 'specialization'>Specialization</p>
+            <p className = 'location'>Location</p>
+         </div>
+      </div>
+   );
+}
+
+export default Background;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+   return (
+      <div className='content'>
+         <div>
+            avatar + description
+            </div>
+         <div>
+            my posts
+               <div>
+                  new post
+               </div>
+            <div>
+               <div>
+                  post1
+               </div>
+               <div>
+                  post2
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Profile;

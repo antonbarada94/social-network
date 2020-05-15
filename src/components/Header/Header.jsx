1c17b1dd6fe9cf84d606b685ad9a69eb428f5c7b
Anton Sergushkin <antonbarada94@gmail.com>
2020-05-15 11:43:43 +0300

    Added header avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from '../../img/logo.png'
+import logo from '../../img/logo.png';
+import headerAvatar from '../../img/post_avatar.png'
 import './Header.css'
 
 const Header = () => {
    return (
       <header className='header'>
-         <img src = {logo} />
+         <img className = 'logo' src = {logo} />
+         <img className = 'avatar' src = {headerAvatar} />
       </header>
    );
 }

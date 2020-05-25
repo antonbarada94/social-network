a434741120198c10ba90c93f8a746cde84aafca9
Anton Sergushkin <antonbarada94@gmail.com>
2020-05-25 23:15:59 +0300

    Create Add post logic

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = (props) => {
             <Background />
             <Navigation />
             <div className='app-wrapper-content'>
-               <Route path = '/profile' render = {() => <Profile state = {props.state.profilePage}/>} />
+               <Route path = '/profile' render = {() => <Profile state = {props.state.profilePage} addPost = {props.addPost} updateNewPostText = {props.updateNewPostText}/>} />
                <Route path = '/dialogs' render = {() => <Dialogs state =  {props.state.messagesPage}/>} />
                <Route path = '/news' render = {() => <News />} />
                <Route path = '/music' render = {() => <Music />} />
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,24 @@ import './MyPosts.css'
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-   let postsElements = props.posts.reverse().map(post => <Post message = {post.message} user = {post.user} date = {post.date} likeCount = {post.likeCount} />)
+
+   let postsElements = props.posts.map(post => <Post message = {post.message} user = {post.user} date = {post.date} likeCount = {post.likeCount} />).reverse();
 
    let newPostElement = React.createRef();
 
    let addPost = () => {
+      props.addPost();
+   }
+
+   let onPostChange = () => {
       let text = newPostElement.current.value;
-      alert(text);
+      props.updateNewPostText(text);
    }
 
    return (
       <div>
          <div className = 'newpost'>
-            <textarea ref = {newPostElement} className = 'newpost-area' placeholder = "What's new?"></textarea>
+            <textarea ref = {newPostElement} className = 'newpost-area' placeholder = "What's new?" onChange = {onPostChange} value = {props.newPostText}></textarea>
             <button className = 'newpost-button' onClick = {addPost}>Post</button>
          </div>
          <div className = 'posts'>

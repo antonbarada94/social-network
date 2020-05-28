7b0b88dbaf29b8b4123e62324069ff0b769d42ea
Anton Sergushkin <antonbarada94@gmail.com>
2020-05-28 18:12:38 +0300

    Aded state logic (messages and posts)

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from "../render";
+
  let state = {
    profilePage: {
       posts: [
@@ -5,6 +7,7 @@
          {id: 2, message: 'Hi, how are you?', user: 'Elizaveta Kaminskaya', date:'10 / 05 / 2020', likeCount: 10},
          {id: 3, message: 'All is good, thanks!', user: 'Anton Sergushkin', date:'11 / 05 / 2020', likeCount: 40},
       ],
+      newPostText: "",
    },
 
    messagesPage: {
@@ -19,11 +22,49 @@
       messages: [
          {id: 1, name: 'Anton', message: 'Hi'},
          {id: 2, name: 'Liza', message: 'How is your barada?'},
+         {id: 2, name: 'Liza', message: 'I love you'},
+         {id: 2, name: 'Liza', message: 'Bubka'},
+         {id: 2, name: 'Liza', message: 'Popkinks'},
          {id: 3, name: 'Sergei', message: 'Whats up dude!'},
          {id: 4, name: 'Nastya', message: 'Hi. How you doing?'},
          {id: 5, name: 'Natasha', message: 'Okay'},
       ],
+      newMessageText: '',
    }
  }
+
+export let addPost = () => {
+   let newPost = {
+      id: 5,
+      message: state.messagesPage.newPostText,
+      user: 'Anton Sergushkin',
+      date: '25 / 05 / 2020',
+      likeCount: 0,
+   };
+   state.profilePage.posts.push(newPost);
+   state.profilePage.newPostText = '';
+   rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+   state.profilePage.newPostText = newText;
+   rerenderEntireTree(state);
+}
+
+export let addMessage = () => {
+   let newMessage = {
+      id: 1,
+      name: 'Anton',
+      message: state.messagesPage.newMessageText,
+   };
+   state.messagesPage.messages.push(newMessage);
+   state.messagesPage.newMessageText = '';
+   rerenderEntireTree(state);
+}
+
+export let updateNewMessageText = (newMessage) => {
+   state.messagesPage.newMessageText = newMessage;
+   rerenderEntireTree(state);
+}
 
  export default state;
